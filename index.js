8c7df65f5f9ff18230002179ecf990489148534d
abhigupta0507 <abhishekgupta39585@gmail.com>
2025-09-18 20:57:06 +0530

    added a route

--- index.js
+++ index.js
@@ -39,6 +39,12 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req,res)=>{
+  res.status(200).json({
+    "status":"ok",
+    "message":"Hello dosto!!"
+  });
+})
 // Health check endpoint
 app.get("/health", (req, res) => {
   res.status(200).json({
